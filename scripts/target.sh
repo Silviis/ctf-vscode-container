7abfcb83c770331dfc0a6354b0ac21b140bd549a
Eetu Silvennoinen <eetu.silvennoinen@gmail.com>
2025-02-18 01:25:13 +0200

    Update dockerfile base package

--- scripts/target.sh
+++ scripts/target.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-TARGET_HOSTNAME_VAR=""
+TARGET_HOSTNAME_VAR="Example"
 TARGET_IP_VAR="0.0.0.0"
-
-if [[ -n "${TARGET_HOSTNAME_VAR}" && -n "${TARGET_IP_VAR}" ]]; then
-    if grep -q "$TARGET_HOSTNAME_VAR" /etc/hosts; then
-        sudo sed -i "s/.*$TARGET_HOSTNAME_VAR/$TARGET_IP_VAR $TARGET_HOSTNAME_VAR/" /etc/hosts
-    else
-        echo "$TARGET_IP_VAR $TARGET_HOSTNAME_VAR" | sudo tee -a /etc/hosts > /dev/null
-    fi
-fi
 
 export TARGET_HOSTNAME=$TARGET_HOSTNAME_VAR
 export TARGET_IP=$TARGET_IP_VAR
+
+if [[ -n "${TARGET_HOSTNAME_VAR}" && -n "${TARGET_IP_VAR}" ]]; then
+    if ! grep -q "$TARGET_HOSTNAME_VAR" /etc/hosts; then
+        echo "$TARGET_IP_VAR $TARGET_HOSTNAME_VAR" | sudo tee -a /etc/hosts > /dev/null
+    fi
+    # Comment the rest if not using htb
+    TARGET_HOSTNAME_VAR_HTB="${TARGET_HOSTNAME_VAR}.htb"
+    export TARGET_HOSTNAME_HTB=$TARGET_HOSTNAME_VAR_HTB
+    if ! grep -q "$TARGET_HOSTNAME_VAR_HTB" /etc/hosts; then
+        echo "$TARGET_IP_VAR $TARGET_HOSTNAME_VAR_HTB" | sudo tee -a /etc/hosts > /dev/null
+    fi
+fi
